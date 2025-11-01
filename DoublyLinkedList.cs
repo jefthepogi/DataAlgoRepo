bd0249fdeaab1a922fbfe172f259889e965cf637
jefthepogi <jefrussel123@gmail.com>
2025-11-01 09:44:34 +0800

    Completed the doubly linked list data structure.

--- DoublyLinkedList.cs
+++ DoublyLinkedList.cs
@@ -26,31 +26,41 @@ namespace Manongas_DataAlgo {
         public void Insert(int index, int value)
         {
             // TODO: Complete the function
-            Node ptr = Head;
-            int idx = 0;
-            while (ptr == null) 
-            {   
-                if (idx == index - 1)
-                {
-                    Node next_node = ptr.NextNode;
-                    Node new_node = new Node(value);
-                    new_node.PreviousNode = ptr;
-                    new_node.Value = value;
-                    next_node.PreviousNode = new_node;
-                    ptr.NextNode = new_node;
-                }
-                ptr = ptr.NextNode;
-                idx++;
+            if (index >= Count || index < 0) // Check if chosen index is within the bounds
+            {
+                Console.WriteLine("\nInsert({0}): Index bound error.", index);
+                return;
             }
+
+            Node ptr = Head; // Initialize current pointer to Head.
+            int idx = 0; // initialize initial index to zero.
+            while (ptr != null) // Iterate if current pointer is not null.
+            {   
+                if (idx == index)  // If the idx matches the index value then do the appropriate swaps.
+                {
+                    Node new_node = new Node(value); // Create a new node.
+                    // Swap the position of current node with the previous
+                    Node prev_node = ptr.PreviousNode;
+                    new_node.NextNode = ptr;
+                    new_node.PreviousNode = prev_node;
+                    ptr.PreviousNode = new_node;
+                    prev_node.NextNode = new_node;
+                    break;
+                }
+                ptr = ptr.NextNode; // Proceed to the next node
+                idx++; // Increment index
+            }
+
+            Count++;
             
         }
 
         public int Get(int index)
         {
             // TODO: Complete the function
-            Node ptr = Head; // Initialize current pointer to Head.
-            int idx = 0; // initialize initial index to zero.
-            while (ptr != null) // Iterate if current pointer is not null.
+            Node ptr = Head; 
+            int idx = 0;
+            while (ptr != null) 
             {
                 if (idx == index) // If the idx matches the index value then return current node's value.
                 {
@@ -65,31 +75,61 @@ namespace Manongas_DataAlgo {
         public void Remove(int value)
         {
             // TODO: Complete the function
+            Node ptr = Head; 
+            int idx = 0;
+            while (ptr != null) 
+            {
+                if (value == ptr.Value) // Check if parameter is equal to the current node value.
+                {
+                    Node prev_node = ptr.PreviousNode;
+                    if (ptr.NextNode != null) // Check if there are dangling list
+                    {
+                        prev_node.NextNode = ptr.NextNode; // Connect that to the previous node.
+                        return;
+                    }
+
+                    prev_node.NextNode = null; // If there are no dangling list, just assign it null.
+                    
+                }
+
+                idx++; // Increment index.
+                ptr = ptr.NextNode; // Proceed to the next node.
+            }
         }
 
         public void RemoveAt(int index)
         {
             // TODO: Complete the function
+            if (index < 0) // This is to allow negative indexing
+            { 
+                index = Count + index;
+            }
+
+            if (index >= Count || index < 0) 
+            {
+                Console.WriteLine("\nRemoveAt({0}): Index bound error.", index);
+                return;
+            }
+
             Node ptr = Head;
             int idx = 0;
             while (ptr != null)
             {
-                Console.WriteLine("wdw");
                 if (idx == index)
                 {
                     Node prev_node = ptr.PreviousNode;
-                    if (prev_node == null) 
+                    if (prev_node == null) // If the previous node is null, then we must be at the head.
                     {
-                        Head = ptr.NextNode;
-                        Count--;
+                        Head = ptr.NextNode; // Replace Head with its proceeding node.
+                        Count--; // Decrement count
                     }
-                    else if (ptr.NextNode != null)
+                    else if (ptr.NextNode != null) // If there are dangling list, connect it
                     {
                         prev_node.NextNode = ptr.NextNode;
                         Count--;
                     }
                     else {
-                        prev_node.NextNode = null;
+                        prev_node.NextNode = null; // Else, just assign it null
                         Count--;
                     }
                     break;
@@ -121,20 +161,43 @@ namespace Manongas_DataAlgo {
         public int GetMin()
         {
             // TODO: Complete the function
-            return -1;
+            Node ptr = Head;
+            int min = Head.Value; // Get first element as base value
+            while (ptr != null)
+            {
+                if (ptr.Value < min) // Check if current node's value is less than current min
+                {
+                    min = ptr.Value; 
+                }
+                ptr = ptr.NextNode;
+            }
+
+            return min;
         }
 
         public int GetMax()
         {
             // TODO: Complete the function
-            return -1;
+            Node ptr = Head;
+            int max = Head.Value;
+            while (ptr != null)
+            {
+                if (ptr.Value > max) // Check if current node's value is larger than current max
+                {
+                    max = ptr.Value;
+                }
+                ptr = ptr.NextNode;
+            }
+
+            return max;
         }
     }
 
     public class Node
     {
         public Node NextNode = null;
-        public Node PreviousNode = null; // Added new field that tracks the previous node.
+        public Node PreviousNode = null; // Added new
+                                         // that tracks the previous node.
         public int Value;
 
         public Node(int value)
@@ -159,11 +222,11 @@ namespace Manongas_DataAlgo {
             PrintList(myList); // 1 2 3 4 5
 
             myList.RemoveAt(0);
-            Console.WriteLine("\nAfter removing index 4, the number of items in the list " + myList.Count);
+            Console.WriteLine("\nAfter removing index 0, the number of items in the list " + myList.Count);
             PrintList(myList); // 2 3 4 5
 
             myList.RemoveAt(-1);
-            Console.WriteLine("\nRemove index 1 from the last item, the number of items in the list " + myList.Count);
+            Console.WriteLine("\nRemove index -1 from the last item, the number of items in the list " + myList.Count);
             PrintList(myList); // 2 3 4 (removing last item)
 
             myList.Insert(2, value: 10);
@@ -172,7 +235,7 @@ namespace Manongas_DataAlgo {
 
             Console.WriteLine("\nSearch 10: " + myList.Search(10)); // 2
             Console.WriteLine("\nMin: " + myList.GetMin()); // 2
-            Console.WriteLine("\nMax: " + myList.GetMin()); // 10
+            Console.WriteLine("\nMax: " + myList.GetMax()); // 10
 
             Console.ReadKey();
         }
